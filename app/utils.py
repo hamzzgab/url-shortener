77d1627f677cf5cdcf7a4624a46a8618e7bc587f
Hamza Gabajiwala <hamzajg16@gmail.com>
2026-04-09 16:19:36 +0100

    fix

--- app/utils.py
+++ app/utils.py
@@ -1,16 +1,17 @@
 class Base62:
-    __CHARS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    chars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    base = 62
 
-    def encoder(self, url_id, base=62):
+    def encoder(self, url_id):
         if url_id < 0:
             raise ValueError("Value less than 0")
-        if url_id < base:
-            return self.__CHARS[url_id]
-        return self.encoder(url_id // base, base) + self.__CHARS[url_id % base]
+        if url_id < self.base:
+            return self.chars[url_id]
+        return self.encoder(url_id // self.base) + self.chars[url_id % self.base]
 
-    def decoder(self, code, base=62):
+    def decoder(self, code):
         res, length = 0, len(code)
         for i, val in enumerate(code):
             power = (length - i - 1)
-            res += self.__CHARS.find(val) * base ** power
+            res += self.chars.find(val) * self.base ** power
         return res
